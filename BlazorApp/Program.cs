0af230eec815ec5b57ae2307c711d48430818053
MARCELO FRANCO <marcelo_dkp@3coracoes.com.br>
2023-04-11 01:45:43 -0300

    primeiro commit projeto blazor

---
+++ BlazorApp/Program.cs
@@ -0,0 +1,45 @@
+using BlazorApp.Helpers;
+using BlazorApp.Services;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace BlazorApp
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+            builder.RootComponents.Add<App>("app");
+
+            builder.Services
+                .AddScoped<IAccountService, AccountService>()
+                .AddScoped<IAlertService, AlertService>()
+                .AddScoped<IHttpService, HttpService>()
+                .AddScoped<ILocalStorageService, LocalStorageService>();
+
+            builder.Services.AddScoped(x => {
+                var apiUrl = new Uri(builder.Configuration["apiUrl"]);
+
+             
+                if (builder.Configuration["fakeBackend"] == "true")
+                {
+                    var fakeBackendHandler = new FakeBackendHandler(x.GetService<ILocalStorageService>());
+                    return new HttpClient(fakeBackendHandler) { BaseAddress = apiUrl };
+                }
+
+                return new HttpClient() { BaseAddress = apiUrl };
+            });
+
+            var host = builder.Build();
+
+            var accountService = host.Services.GetRequiredService<IAccountService>();
+            await accountService.Initialize();
+
+            await host.RunAsync();
+        }
+    }
+}
